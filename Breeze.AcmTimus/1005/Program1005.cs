256a55e164a91cd447311b62ad380ba6ea67a140
Konstantin <konstantin.chernyaev@list.ru>
2013-12-06 20:18:46 +0400

    1005 init

---
+++ Breeze.AcmTimus/1005/Program1005.cs
@@ -0,0 +1,79 @@
+﻿#region usings
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+#endregion
+
+
+
+namespace _1005
+{
+    class Program1005
+    {
+        #region console
+#if ONLINE_JUDGE
+        static string ReadAll()
+        {
+            return Console.In.ReadToEnd();
+        }
+        static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+#else
+        static string ReadAll()
+        {
+            return File.ReadAllText(InputFilePath);
+        }
+
+
+        static int LineIndex = 0;
+
+
+        static string ReadLine()
+        {
+            string[] lines = File.ReadAllLines(InputFilePath);
+            if (LineIndex < lines.Length)
+            {
+                string rv = lines[LineIndex++];
+                rv = rv.Trim();
+                int i = rv.LastIndexOf("//");
+                if (i >= 0)
+                    return rv.Substring(0, i).Trim();
+                return rv;
+            }
+
+            return "";
+        }
+
+
+        static string InputFilePath { get { return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Input.txt"); } }
+#endif
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            string countStr = ReadLine();
+
+            int[] ws = ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+
+
+#if ONLINE_JUDGE
+#else
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+#endif
+        }
+    }
+}
