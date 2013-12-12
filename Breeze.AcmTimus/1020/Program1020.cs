22ee7560b20c05e54cfcb2889b8104f1b91e2540
Konstantin <konstantin.chernyaev@list.ru>
2013-12-12 19:29:29 +0400

    1020 init

---
+++ Breeze.AcmTimus/1020/Program1020.cs
@@ -0,0 +1,120 @@
+﻿#region usings
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+#endregion
+
+
+
+namespace _1020
+{
+    class Program1020
+    {
+        #region console
+#if ONLINE_JUDGE
+        static string ReadAll()
+        {
+            return Console.In.ReadToEnd();
+        }
+        static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+#else
+        static string ReadAll()
+        {
+            return File.ReadAllText(InputFilePath);
+        }
+
+
+        static int LineIndex = 0;
+
+
+        static string ReadLine()
+        {
+            string[] lines = File.ReadAllLines(InputFilePath);
+            if (LineIndex < lines.Length)
+            {
+                string rv = lines[LineIndex++];
+                rv = rv.Trim();
+                int i = rv.LastIndexOf("//");
+                if (i >= 0)
+                    return rv.Substring(0, i).Trim();
+                return rv;
+            }
+
+            return "";
+        }
+
+
+        static string InputFilePath { get { return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Input.txt"); } }
+#endif
+
+
+        static int[] ReadIntArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+        }
+
+
+        static decimal[] ReadDecimalArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => decimal.Parse(x)).ToArray();
+        }
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+#if ONLINE_JUDGE
+#else
+            do
+            {
+#endif
+                decimal[] a = ReadDecimalArray();
+#if ONLINE_JUDGE
+#else
+                if (a.Length < 2)
+                    break;
+#endif
+                int n = (int) a[0];
+                decimal r = a[1];
+
+                P[] ps = new P[n];
+                for (int i = 0; i < n; i++)
+                {
+                    a = ReadDecimalArray();
+                    ps[i] = new P {X = a[0], Y = a[1]};
+                }
+
+#if ONLINE_JUDGE
+#else
+            } while (true);
+#endif
+
+#if ONLINE_JUDGE
+#else
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+#endif
+        }
+    }
+
+
+
+    class P
+    {
+        public decimal X;
+        public decimal Y;
+    }
+}
