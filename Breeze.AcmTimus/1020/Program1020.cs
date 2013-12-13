5aea6578ff64f1ad38ab7ddfba5958f00054c8a5
Konstantin <konstantin.chernyaev@list.ru>
2013-12-13 17:23:25 +0400

    1020 accepted

--- Breeze.AcmTimus/1020/Program1020.cs
+++ Breeze.AcmTimus/1020/Program1020.cs
@@ -1,6 +1,7 @@
 ﻿#region usings
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -88,15 +89,17 @@ namespace _1020
                     break;
 #endif
                 int n = (int) a[0];
-                decimal r = a[1];
+                double r = (double) a[1];
 
                 P[] ps = new P[n];
                 for (int i = 0; i < n; i++)
                 {
                     a = ReadDecimalArray();
-                    ps[i] = new P {X = a[0], Y = a[1]};
+                    ps[i] = new P {X = (double) a[0], Y = (double) a[1]};
                 }
 
+                double res = Solve(ps, r);
+                Console.WriteLine(Math.Round(res, 2));
 #if ONLINE_JUDGE
 #else
             } while (true);
@@ -108,13 +111,52 @@ namespace _1020
             Console.ReadLine();
 #endif
         }
+
+
+        static double Solve(P[] ps, double r)
+        {
+            var psEx = new P[ps.Length + 2];
+            psEx[0] = ps[ps.Length - 1];
+            psEx[ps.Length + 1] = ps[0];
+            for (int i = 0; i < ps.Length; i++)
+                psEx[i + 1] = ps[i];
+
+            double strightsSum = 0;
+            for (int i = 1; i <= ps.Length; i++)
+                strightsSum += Stright(psEx[i - 1], psEx[i]);
+
+            double rSum = 2*r*Math.PI;
+//            for (int i = 1; i <= ps.Length; i++)
+//                rSum += R(psEx[i - 1], psEx[i], psEx[i + 1], r);
+
+            return strightsSum + rSum;
+        }
+
+
+        static double R(P p, P c, P n, double r)
+        {
+            return r*(Math.PI - Math.Acos(ScalarMult(p, c, n)/Stright(p, c)/Stright(c, n)));
+        }
+
+
+        static double ScalarMult(P p, P c, P n)
+        {
+            return (p.X - c.X)*(n.X - c.X) + (p.Y - c.Y)*(n.Y - c.Y);
+        }
+
+
+        static double Stright(P a, P b)
+        {
+            return Math.Sqrt((a.X - b.X)*(a.X - b.X) + (a.Y - b.Y)*(a.Y - b.Y));
+        }
     }
 
 
 
+    [DebuggerDisplay("{X} {Y}")]
     class P
     {
-        public decimal X;
-        public decimal Y;
+        public double X;
+        public double Y;
     }
 }
