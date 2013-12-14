8a56c4c27cabd60e06905d34af2bb1df6cfaec30
Konstantin Chernyaev <konstantin.chernyaev@list.ru>
2013-12-14 20:46:34 +0400

    1017 accepted

--- Breeze.AcmTimus/1017/Program1017.cs
+++ Breeze.AcmTimus/1017/Program1017.cs
@@ -16,149 +16,153 @@ using System.Threading.Tasks;
 
 namespace _1017
 {
-    class Program1017
-    {
-        #region console
-#if ONLINE_JUDGE
-        static string ReadAll()
-        {
-            return Console.In.ReadToEnd();
-        }
-        static string ReadLine()
-        {
-            return Console.ReadLine();
-        }
-#else
-        static string ReadAll()
-        {
-            return File.ReadAllText(InputFilePath);
-        }
+	class Program1017
+	{
+		#region console
+		#if ONLINE_JUDGE
+		static string ReadAll()
+		{
+			return Console.In.ReadToEnd();
+		}
+		static string ReadLine()
+		{
+			return Console.ReadLine();
+		}
+		#else
+		static string ReadAll()
+		{
+			return File.ReadAllText(InputFilePath);
+		}
 
 
-        static int LineIndex = 0;
+		static int LineIndex = 0;
 
 
-        static string ReadLine()
-        {
-            string[] lines = File.ReadAllLines(InputFilePath);
-            if (LineIndex < lines.Length)
-            {
-                string rv = lines[LineIndex++];
-                if (rv.StartsWith("//"))
-                    return ReadLine();
+		static string ReadLine()
+		{
+			string[] lines = File.ReadAllLines(InputFilePath);
+			if (LineIndex < lines.Length)
+			{
+				string rv = lines[LineIndex++];
+				if (rv.StartsWith("//"))
+					return ReadLine();
 
-                rv = rv.Trim();
-                int i = rv.LastIndexOf("//");
-                if (i >= 0)
-                    return rv.Substring(0, i).Trim();
-                return rv;
-            }
+				rv = rv.Trim();
+				int i = rv.LastIndexOf("//");
+				if (i >= 0)
+					return rv.Substring(0, i).Trim();
+				return rv;
+			}
 
-            return "";
-        }
-
-
-        static string InputFilePath { get { return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Input.txt"); } }
-#endif
+			return "";
+		}
 
 
-        static int[] ReadIntArray()
-        {
-            string s = ReadLine();
-            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
-        }
+		static string InputFilePath { get { return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Input.txt"); } }
+		#endif
 
 
-        static decimal[] ReadDecimalArray()
-        {
-            string s = ReadLine();
-            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => decimal.Parse(x)).ToArray();
-        }
+		static int[] ReadIntArray()
+		{
+			string s = ReadLine();
+			return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+		}
 
 
-        static int ReadLineInt()
-        {
-            return int.Parse(ReadLine());
-        }
-        #endregion
+		static decimal[] ReadDecimalArray()
+		{
+			string s = ReadLine();
+			return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => decimal.Parse(x)).ToArray();
+		}
 
 
-        static void Main(string[] args)
-        {
-            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-#if ONLINE_JUDGE
-#else
-            do
-            {
-#endif
-                int n = ReadLineInt();
-
-#if ONLINE_JUDGE
-#else
-                if (n < 0)
-                    break;
-#endif
-
-                long res = Solve(n);
-#if ONLINE_JUDGE
-                Console.WriteLine(res);
-#else
-                Console.WriteLine(string.Format("{0} : {1}", n, res));
-#endif
-
-#if ONLINE_JUDGE
-#else
-            } while (true);
-#endif
-
-#if ONLINE_JUDGE
-#else
-            Console.WriteLine("Press Enter to exit");
-            Console.ReadLine();
-#endif
-        }
+		static int ReadLineInt()
+		{
+			return int.Parse(ReadLine());
+		}
+		#endregion
 
 
-        /*
-         * <=2 => 0
-         * 3,4 => 1
-         * */
+		static void Main(string[] args)
+		{
+			Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+			#if ONLINE_JUDGE
+			#else
+			do
+			{
+				#endif
+				int n = ReadLineInt();
+
+				#if ONLINE_JUDGE
+				#else
+				if (n < 0)
+					break;
+				#endif
+
+				long res = Solve(n);
+				#if ONLINE_JUDGE
+				Console.WriteLine(res);
+				#else
+				Console.WriteLine(string.Format("{0} : {1}", n, res));
+				#endif
+
+				#if ONLINE_JUDGE
+				#else
+			} while (true);
+			#endif
+
+			#if ONLINE_JUDGE
+			#else
+			Console.WriteLine("Press Enter to exit");
+			Console.ReadLine();
+			#endif
+		}
 
 
-        static long Solve(int n)
-        {
-            if (n == 0)
-                return 0;
-
-            long rv = 0;
-            for (int first = 1; first <= n/2; first++)
-                rv += L(n, first);
-
-            return rv;
-        }
+		/*
+		 * <=2 => 0
+		 * 3,4 => 1
+		 * */
 
 
-        static long L(int total, int first)
-        {
-            int left = total - first;
+		static long Solve(int n)
+		{
+			if (n == 0)
+				return 0;
 
-            if (left < 0)
-                return 0;
-            if (left == 0)
-                return 1;
-            if (left < first + 1)
-                return 0;
+			cache= new long[n+1,n+1];
+			b = new bool[n+1,n+1];
+			 
+			long rv = 0;
+			for (int first = 1; first <= n/2; first++)
+				rv += L(first, n );
 
-            if (left <= first + 2)
-                return 1;
+			return rv;
+		}
+		
+		static long[,] cache;
+		static bool[,] b;
 
-            long sum = 0;
 
-            for (int current = first; current <= total/2; current++)
-                sum += L(total - current, current + 1);
+		static long L(int first, int n)
+		{
+			if(n<first)
+				return 0;
+			if(n==first)
+				return 1;
+			
+			if(b[first,n])
+				return cache[first,n];
+			long sum = 0;
+			
+			int left=n-first;
 
-            sum++; // current==total, all in one tower
-            return sum;
-        }
-    }
+			for (int next = first+1; next <= left; next++)
+				sum += L(next, left);
+
+			cache[first,n]=sum;
+			b[first,n]=true;
+			return sum;
+		}
+	}
 }
