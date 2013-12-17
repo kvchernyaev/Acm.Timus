50ad21904a856afa0ec61ec2b791e3814148e538
Konstantin <konstantin.chernyaev@list.ru>
2013-12-17 17:25:14 +0400

    13_1210 accepted

--- Breeze.AcmTimus/13_1210_170/Program_13_1210_170.cs
+++ Breeze.AcmTimus/13_1210_170/Program_13_1210_170.cs
@@ -65,34 +65,35 @@ namespace _13_1210_170
         static int[] ReadIntArray()
         {
             string s = ReadLine();
-            return s.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
         }
 
 
         static long[] ReadLongArray()
         {
             string s = ReadLine();
-            return s.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToArray();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToArray();
         }
 
 
         static ulong[] ReadUlongArray()
         {
             string s = ReadLine();
-            return s.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(x => ulong.Parse(x)).ToArray();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => ulong.Parse(x)).ToArray();
         }
 
 
         static decimal[] ReadDecimalArray()
         {
             string s = ReadLine();
-            return s.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(x => decimal.Parse(x)).ToArray();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(x => decimal.Parse(x)).ToArray();
         }
 
 
         static int ReadLineInt()
         {
-            return int.Parse(ReadLine());
+            string s = ReadLine();
+            return int.Parse(s);
         }
 
 
@@ -117,15 +118,46 @@ namespace _13_1210_170
             do
             {
 #endif
-                long[] ar = ReadLongArray();
+                int levels = ReadLineInt();
 
 #if ONLINE_JUDGE
 #else
-                if (ar.Length <= 1)
+                if (levels <= 0)
                     break;
 #endif
 
-                decimal res = Solve(ar);
+                // level first -> i1 -> i2 -> cost
+                Dictionary<int, int>[][] d = new Dictionary<int, int>[levels][];
+
+                int prevplanets = 1;
+                for (int l = 0; l < levels; l++)
+                {
+                    int planets = ReadLineInt();
+                    d[l] = new Dictionary<int, int>[prevplanets];
+
+                    int curPlanet = 0; // 0-based
+                    for (int asf = 0; asf < planets; asf++)
+                    {
+                        int[] ar = ReadIntArray();
+                        for (int i = 0; i < ar.Length/2; i++)
+                        {
+                            int prevPlanet = ar[2*i] - 1; // 0-based
+                            int cost = ar[2*i + 1];
+
+                            if (d[l][prevPlanet] == null)
+                                d[l][prevPlanet] = new Dictionary<int, int>();
+                            d[l][prevPlanet][curPlanet] = cost;
+                        }
+                        curPlanet++;
+                    }
+
+                    if (l < levels - 1)
+                        ReadLine();
+
+                    prevplanets = planets;
+                }
+
+                int res = Solve(d);
 #if ONLINE_JUDGE
                 Console.WriteLine(res);
 #else
@@ -145,6 +177,50 @@ namespace _13_1210_170
         }
 
 
+        static int Solve(Dictionary<int, int>[][] dGeneral)
+        {
+            // [planet1 -> planet2 -> cost]
+            _cache = new Dictionary<KeyValuePair<int, int>, int>();
+            _dGeneral = dGeneral;
+            _levels = dGeneral.Length;
+
+            return Solve(0, 0);
+        }
+
+
+        static Dictionary<int, int>[][] _dGeneral;
+        static int _levels;
+
+        static Dictionary<KeyValuePair<int, int>, int> _cache;
+
+
+        static int Solve(int curLevel, int curPlanet)
+        {
+            var kvp = new KeyValuePair<int, int>(curLevel, curPlanet);
+            int rv;
+            if (_cache.TryGetValue(kvp, out rv))
+                return rv;
+
+            Dictionary<int, int>[] planets = _dGeneral[curLevel];
+
+            Dictionary<int, int> edges = planets[curPlanet];
+
+            int bestLeftCost = int.MaxValue;
+
+            foreach (int nextPlanet in edges.Keys)
+            {
+                int cost = edges[nextPlanet];
+
+                int leftCost = curLevel == _levels - 1
+                                   ? cost
+                                   : (cost + Solve(curLevel + 1, nextPlanet));
+
+                if (leftCost < bestLeftCost)
+                    bestLeftCost = leftCost;
+            }
+
+            _cache.Add(kvp, bestLeftCost);
+            return bestLeftCost;
+        }
     }
 }
-
