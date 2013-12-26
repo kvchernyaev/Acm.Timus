48557ccf3141f6849f15c630e8453b3a0dfecb45
Konstantin <konstantin.chernyaev@list.ru>
2013-12-26 14:18:32 +0400

    19_1680 accepted

---
+++ Breeze.AcmTimus/19_1680_192/Program_19_1680_192.cs
@@ -0,0 +1,257 @@
+﻿#region usings
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+#endregion
+
+
+
+namespace _19_1680_192
+{
+    class Program_19_1680_192
+    {
+        #region console
+#if ONLINE_JUDGE
+        static string ReadAll()
+        {
+            return Console.In.ReadToEnd();
+        }
+
+
+        static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+
+        static char ReadChar()
+        {
+            int code = Console.Read();
+            if (code < 0)
+                return (char) 26;
+            return (char) code;
+            ConsoleKeyInfo key = Console.ReadKey(false);
+            return key.KeyChar;
+        }
+#else
+        static string ReadAll()
+        {
+            return File.ReadAllText(InputFilePath);
+        }
+
+
+        static int LineIndex = 0;
+        static int CharIndex = 0;
+
+
+        static string ReadLine()
+        {
+            string[] lines = File.ReadAllLines(InputFilePath);
+            if (LineIndex < lines.Length)
+            {
+                string rv = lines[LineIndex++];
+                if (rv.StartsWith("//"))
+                    return ReadLine();
+
+                rv = rv.Trim();
+                int i = rv.LastIndexOf("//");
+                if (i >= 0)
+                    return rv.Substring(0, i).Trim();
+                return rv;
+            }
+
+            return "";
+        }
+
+
+        static char ReadChar()
+        {
+            string[] lines = File.ReadAllLines(InputFilePath);
+            if (LineIndex < lines.Length)
+            {
+                string rv = lines[LineIndex];
+                if (rv.StartsWith("//"))
+                    rv = ReadLine();
+
+                rv = rv.Trim();
+                int i = rv.LastIndexOf("//");
+                if (i >= 0)
+                    rv = rv.Substring(0, i).Trim();
+
+                if (CharIndex < rv.Length)
+                    return rv[CharIndex++];
+
+                CharIndex = 0;
+                LineIndex++;
+                return '\n';
+            }
+
+            return '\n';
+        }
+
+
+        static string InputFilePath { get { return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Input.txt"); } }
+#endif
+
+
+        #region read specific
+        static string ReadLineTrim()
+        {
+            return ReadLine().Trim();
+        }
+
+
+        static int[] ReadIntArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+
+
+        static long[] ReadLongArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+        }
+
+
+        static ulong[] ReadUlongArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse).ToArray();
+        }
+
+
+        static decimal[] ReadDecimalArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).ToArray();
+        }
+
+
+        static int ReadIntLine()
+        {
+            string s = ReadLine();
+            return int.Parse(s);
+        }
+
+
+        static int? ReadIntNLine()
+        {
+            string s = ReadLine();
+            s = s.TrimStart((char) 0xEF, (char) 0xBB, (char) 0xBf, (char) 1103, (char) 9559, (char) 9488);
+
+            if (string.IsNullOrEmpty(s))
+                return null;
+            return int.Parse(s);
+        }
+
+
+        static decimal ReadDecimalLine()
+        {
+            string s = ReadLine();
+            return decimal.Parse(s);
+        }
+
+
+        static long ReadLongLine()
+        {
+            return long.Parse(ReadLine());
+        }
+
+
+        static ulong ReadUlongLine()
+        {
+            return ulong.Parse(ReadLine());
+        }
+        #endregion
+
+
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+#if ONLINE_JUDGE
+#else
+            do
+            {
+#endif
+                string s = ReadLine();
+#if ONLINE_JUDGE
+#else
+                if (s == "")
+                    break;
+#endif
+                int[] ar = s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int n = ar[1];
+                int q = ar[2];
+
+                string[] vuzes = new string[q];
+
+                int currentPlace = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    Tuple<string, string> t = Parse(ReadLine());
+
+
+                    string vuz = t.Item2;
+
+                    bool was = false;
+                    for (int j = 0; j < currentPlace; j++)
+                        if (string.Compare(vuzes[j], vuz, StringComparison.InvariantCultureIgnoreCase) == 0)
+                        {
+                            was = true;
+                            break;
+                        }
+
+                    if (was)
+                        continue;
+                 
+                    if (currentPlace == q)
+                    {
+                        Console.WriteLine(t.Item1);
+                        for (int k = i + 1; k < n; k++)
+                            ReadLine();
+                        break;
+                    }
+
+                    vuzes[currentPlace++] = vuz;
+                }
+
+#if ONLINE_JUDGE
+#else
+            } while (true);
+#endif
+
+#if ONLINE_JUDGE
+#else
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+#endif
+        }
+
+
+        static Tuple<string, string> Parse(string s)
+        {
+            string vuz;
+
+            int ind = s.LastIndexOf('#');
+            if (ind < 0)
+                vuz = s.Trim();
+            else
+                vuz = s.Substring(0, ind).Trim();
+
+            return new Tuple<string, string>(s, vuz);
+        }
+    }
+}
