ff491094df06ef8be5f219016420cd0966fb796e
Konstantin <konstantin.chernyaev@list.ru>
2014-02-03 14:12:15 +0400

    25_1586 accepted (plagiat)

--- Breeze.AcmTimus/25_1586_201/Program_25_1586_201.cs
+++ Breeze.AcmTimus/25_1586_201/Program_25_1586_201.cs
@@ -181,6 +181,15 @@ namespace _25_1586_201
         #endregion
 
 
+        static void Log(string s, params object[] objs)
+        {
+#if ONLINE_JUDGE
+#else
+            Console.WriteLine(string.Format(s, objs));
+#endif
+        }
+
+
         static uint[] primes3 = new uint[143]
         {
             101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
@@ -193,10 +202,10 @@ namespace _25_1586_201
             739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829,
             839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941,
             947, 953, 967, 971, 977, 983, 991, 997
-        };
+        }; // 11 with 0 in the middle
 
 
-        static uint mod = 1000000009;
+        static ulong mod = 1000000009;
 
 
         static void Main(string[] args)
@@ -207,11 +216,21 @@ namespace _25_1586_201
             do
             {
 #endif
-                int? countN = ReadIntNLine();
+                int? nN = ReadIntNLine();
 #if ONLINE_JUDGE
 #else
-                if (countN == null)
+                if (nN == null)
                     break;
+#endif
+
+                int n = nN.Value; // 3 <= n <= 10 000
+
+                ulong rv = SolveMatrix(n);
+
+#if ONLINE_JUDGE
+                Console.WriteLine(rv);
+#else
+                Console.WriteLine(string.Format("{1} : {0}", rv, n));
 #endif
 
 #if ONLINE_JUDGE
@@ -224,6 +243,153 @@ namespace _25_1586_201
             Console.WriteLine("Press Enter to exit");
             Console.ReadLine();
 #endif
+        }
+
+
+        static List<int>[] nextPrimes3;
+
+
+        static ulong Solve(int n)
+        {
+            //uint[] ordered = primes3.OrderBy(p => p%100).ToArray();
+
+            nextPrimes3 = new List<int>[primes3.Length];
+
+            for (int i = 0; i < primes3.Length; i++)
+            {
+                nextPrimes3[i] = new List<int>();
+
+                for (int j = 0; j < primes3.Length; j++)
+                    if (primes3[i]%100 == primes3[j]/10)
+                        nextPrimes3[i].Add(j);
+            }
+
+            int count = n - 3;
+
+            ulong total = 0;
+
+            for (int i = 0; i < primes3.Length; i++)
+            {
+                ulong forOne = Recurr(i, count);
+                total += forOne;
+                if (total > mod)
+                    total %= mod;
+            }
+
+            return total;
+        }
+
+
+        static ulong Recurr(int pIndex, int count)
+        {
+            if (count == 0)
+                return 1;
+
+            List<int> next = nextPrimes3[pIndex];
+
+            if (count == 1)
+                return (ulong) next.Count;
+
+            count--;
+
+            ulong total = 0;
+            foreach (int nextIndex in next)
+            {
+                ulong forOne = Recurr(nextIndex, count);
+                total += forOne;
+                if (total >= mod)
+                    total %= mod;
+            }
+
+            return total;
+        }
+
+
+        static ulong SolveMatrix(int n)
+        {
+            ulong[,] matrix = new ulong[143,143];
+
+            for (int i = 0; i < 143; i++)
+                for (int j = 0; j < 143; j++)
+                    matrix[i, j] = (ulong) (primes3[i]%100 == primes3[j]/10 ? 1 : 0);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            ulong[,] res = Pow(matrix, n - 3);
+            sw.Stop();
+            Log("_ " + sw.ElapsedMilliseconds);
+
+            ulong rv = Sum(res);
+
+            return rv;
+        }
+
+
+        static ulong Sum(ulong[,] m)
+        {
+            ulong rv = 0;
+            foreach (ulong v in m)
+            {
+                rv += v;
+                if (rv > mod)
+                    rv %= mod;
+            }
+            return rv;
+        }
+
+
+        static ulong[,] Pow(ulong[,] m, int pow)
+        {
+            if (pow == 0)
+                return Unity();
+            if (pow == 1)
+                return m;
+
+//            var res = (ulong[,]) m.Clone();
+//            for (int p = 0; p < pow - 1; p++)
+//                res = Mult(res, m);
+//            return res;
+
+            int n = pow;
+            ulong[,] curPowed = m;
+            ulong[,] res = Unity();
+
+            while (n > 0)
+            {
+                if (n%2 == 1)
+                    res = Mult(res, curPowed);
+                n /= 2;
+                curPowed = Mult(curPowed, curPowed);
+            }
+
+            return res;
+        }
+
+
+        static ulong[,] Unity()
+        {
+            ulong[,] unity = new ulong[primes3.Length,primes3.Length];
+            for (int i = 0; i < 143; i++)
+                unity[i, i] = 1;
+            return unity;
+        }
+
+
+        static ulong[,] Mult(ulong[,] m1, ulong[,] m2)
+        {
+            ulong[,] res = new ulong[143,143];
+
+            for (int i = 0; i < 143; i++)
+                for (int j = 0; j < 143; j++)
+                {
+                    ulong val = 0;
+                    for (int k = 0; k < 143; k++)
+                        val += m1[i, k]*m2[k, j];
+                    if (val > mod)
+                        val %= mod;
+                    res[i, j] = val;
+                }
+            return res;
         }
     }
 }
