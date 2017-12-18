55c70cda8526888b3ab885b71429cfae0937c133
cherniaevkv <cherniaevkv@suek.ru>
2017-12-18 19:03:29 +0300

    1025. Демократия в опасности accepted

---
+++ Breeze.AcmTimus/Timus1025/Program1025.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+
+namespace Timus1025
+{
+    // http://acm.timus.ru/problem.aspx?space=1&num=1025
+    
+    internal class Program
+    {
+        
+        #region console
+#if ONLINE_JUDGE
+        static string ReadAll()
+        {
+            return Console.In.ReadToEnd();
+        }
+
+
+        static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+
+        static char ReadChar()
+        {
+            int code = Console.Read();
+            if (code < 0)
+                return (char) 26;
+            return (char) code;
+            ConsoleKeyInfo key = Console.ReadKey(false);
+            return key.KeyChar;
+        }
+#else
+        static string ReadAll()
+        {
+            return File.ReadAllText(InputFilePath);
+        }
+
+
+        static int LineIndex = 0;
+        static int CharIndex = 0;
+
+
+        static string ReadLine()
+        {
+            string[] lines = File.ReadAllLines(InputFilePath);
+            if (LineIndex < lines.Length)
+            {
+                string rv = lines[LineIndex++];
+                if (rv.StartsWith("//"))
+                    return ReadLine();
+
+                rv = rv.Trim();
+                int i = rv.LastIndexOf("//");
+                if (i >= 0)
+                    return rv.Substring(0, i).Trim();
+                return rv;
+            }
+
+            return "";
+        }
+
+
+        static char ReadChar()
+        {
+            string[] lines = File.ReadAllLines(InputFilePath);
+            if (LineIndex < lines.Length)
+            {
+                string rv = lines[LineIndex];
+                if (rv.StartsWith("//"))
+                    rv = ReadLine();
+
+                rv = rv.Trim();
+                int i = rv.LastIndexOf("//");
+                if (i >= 0)
+                    rv = rv.Substring(0, i).Trim();
+
+                if (CharIndex < rv.Length)
+                    return rv[CharIndex++];
+
+                CharIndex = 0;
+                LineIndex++;
+                return '\n';
+            }
+
+            return '\n';
+        }
+
+
+        static string InputFilePath { get { return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Input.txt"); } }
+#endif
+
+
+        #region read specific
+        static string ReadLineTrim()
+        {
+            return ReadLine().Trim();
+        }
+
+
+        static int[] ReadIntArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+
+
+        static long[] ReadLongArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+        }
+
+
+        static ulong[] ReadUlongArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse).ToArray();
+        }
+
+
+        static decimal[] ReadDecimalArray()
+        {
+            string s = ReadLine();
+            return s.Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).ToArray();
+        }
+
+
+        static int ReadIntLine()
+        {
+            string s = ReadLine();
+            return int.Parse(s);
+        }
+
+
+        static int? ReadIntNLine()
+        {
+            string s = ReadLine();
+            s = s.TrimStart((char) 0xEF, (char) 0xBB, (char) 0xBf, (char) 1103, (char) 9559, (char) 9488);
+
+            if (string.IsNullOrEmpty(s))
+                return null;
+            return int.Parse(s);
+        }
+
+
+        static decimal ReadDecimalLine()
+        {
+            string s = ReadLine();
+            return decimal.Parse(s);
+        }
+
+
+        static long ReadLongLine()
+        {
+            return long.Parse(ReadLine());
+        }
+
+
+        static ulong ReadUlongLine()
+        {
+            return ulong.Parse(ReadLine());
+        }
+        #endregion
+
+
+        #endregion
+
+
+        static void Log(string s, params object[] objs)
+        {
+#if ONLINE_JUDGE
+#else
+            Console.WriteLine(string.Format(s, objs));
+#endif
+        }
+
+        
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            int groupCount = ReadIntLine();
+            int[] groups = ReadIntArray();
+            Array.Sort(groups);
+            int neededCount = groups.Length / 2 + 1;
+            int rv = groups.Take(neededCount).Select(x => x / 2 + 1).Sum();
+            
+            Console.WriteLine(rv);
+        }
+    }
+}
